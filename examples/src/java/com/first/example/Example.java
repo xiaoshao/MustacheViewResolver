f3344c3bb3f1c39f8a542d91e1a1e26037c353a5
psw-jade team <psw-jade@rea.com>
2016-09-05 15:07:56 +0800

    Init multi project with gradle.

---
+++ examples/src/java/com/first/example/Example.java
@@ -0,0 +1,11 @@
+package com.first.example;
+
+import com.view.resolver.ViewResolver;
+
+public class Example {
+
+    public static void main(String[] args){
+        ViewResolver viewResolver = new ViewResolver();
+        viewResolver.test();
+    }
+}
---
+++ viewResolver/src/main/java/com/view/resolver/ViewResolver.java
@@ -0,0 +1,11 @@
+package com.view.resolver;
+
+/**
+ * Created by zwshao on 9/5/16.
+ */
+public class ViewResolver {
+
+    public void test(){
+        System.out.println("Hello world");
+    }
+}
