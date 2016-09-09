d6e940452a9fca5e9addc4a49a9b634e17573065
psw-jade team <psw-jade@rea.com>
2016-09-09 11:09:33 +0800

    Use thymelead template.

---
+++ examples/src/main/java/com/config/ThymeleafConfig.java
@@ -0,0 +1,51 @@
+package com.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+@EnableWebMvc
+public class ThymeleafConfig implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public ViewResolver thymeleafViewResolver(TemplateEngine templateEngine) {
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(templateEngine);
+        return thymeleafViewResolver;
+    }
+
+    @Bean
+    public TemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        return templateResolver;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
--- examples/src/main/java/com/config/WebApplicationInitializer.java
+++ examples/src/main/java/com/config/WebApplicationInitializer.java
@@ -10,8 +10,7 @@ public class WebApplicationInitializer extends AbstractAnnotationConfigDispatche
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        System.out.println("kkkk");
-        return new Class<?>[]{WebConfig.class};
+        return new Class<?>[]{WebConfig.class, ThymeleafConfig.class};
     }
 
     @Override
